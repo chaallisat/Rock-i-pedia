82914575b6ccae5bc52cf4255d54efd15b71ae52
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-08-01 16:20:23 -0700

    added more information to objects

--- assets/javascript/bandAPI.js
+++ assets/javascript/bandAPI.js
@@ -3,6 +3,7 @@ const bands = {
         paramore: {
             name: "Paramore",
             members: 5,
+            artist: [],
             bestsong: "Misery Buisness",
             url: "find one",
             image: "https://i.ytimg.com/vi/aCyGvGEtOwc/maxresdefault.jpg"
@@ -10,6 +11,7 @@ const bands = {
         icon4hire: {
             name: "Icon for Hire",
             members: 3,
+            artist: [],
             bestsong: "Make a Move",
             url: "find one",
             image: "https://images.genius.com/813ec3e48552cb122ad19a0614641847.1000x1000x1.jpg"
@@ -19,6 +21,7 @@ const bands = {
         greenday: {
             name: "Green Day",
             members: 3,
+            artist: ["Billie-Joe Armstrong"],
             bestsong: "American Idiot",
             url: "find one",
             image: "find it"
@@ -26,6 +29,7 @@ const bands = {
         mayday: {
             name: "Mayday Parade",
             members: 6,
+            artist: [],
             bestsong: "Terrible Things",
             url: "find one",
             image: "find it"
@@ -33,13 +37,17 @@ const bands = {
     },
     alt: {
         ptv: {
+            name: "Pierce the Veil",
             members: 4,
+            artist: ["Vic Fuentes", "Mike Fuentes", "Jaime Percaudo", "Tony Perry"],
             bestsong: "King for a Day",
             url: "find one",
             image: "find it"
         },
         sws: {
+            name: "Sleeping with Sirens",
             members: 5,
+            artist: [],
             bestsong: "Kick Me",
             url: "find one",
             image: "find it"
